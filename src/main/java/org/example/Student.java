3377af0396a0808bd64ab28c95fa71c863a06f45
alassane seck <aseck9719@gmail.com>
2025-08-03 22:13:09 +0200

    getter and setter

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -11,14 +11,8 @@ public class Student {
     public String getFirstName() {
         return firstName;
     }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
     public String getLastName() {
         return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     @Override
